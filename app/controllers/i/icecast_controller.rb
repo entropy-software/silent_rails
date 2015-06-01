d2642213f781cc0bba16a9499b686a6ce7d33230
Xavier Banegas <xbanegas@gmail.com>
2015-06-01 16:46:16 -0400

    working minimal iceserv endpoints

--- app/controllers/i/icecast_controller.rb
+++ app/controllers/i/icecast_controller.rb
@@ -1,18 +1,34 @@
 class I::IcecastController < ApplicationController
 	skip_before_action :verify_authenticity_token
+	before_action :authenticate_ice
 
 	def notify_mount
-		if request.GET[:action] == 'mount_add'
+		if request.POST[:action] == 'mount_add'
+			response.headers['icecast-auth-user'] = '1'
+			render :text => "ok", :status => 200
 		end		
-		if request.GET[:action] == 'mount_remove'
+		if request.POST[:action] == 'mount_remove'
+			response.headers['icecast-auth-user'] = '1'
+			render :text => "ok", :status => 200
 		end
 	end
 
 	def notify_listener
-		if request.GET[:action] == 'listener_add'
+		if request.POST[:action] == 'listener_add'
+			response.headers['icecast-auth-user'] = '1'
+			render :text => "ok", :status => 200
 		end
-		if request.GET[:action] == 'listener_remove'
+		if request.POST[:action] == 'listener_remove'
+			response.headers['icecast-auth-user'] = '1'
+			render :text => "ok", :status => 200
 		end
 	end
 
+  private
+    def authenticate_ice
+    	# make sure request comes from icecast ip
+    	# logger.debug request.ip
+    	# make sure session is existing user session
+    	# logger.debug session[:session_id]
+    end
 end
